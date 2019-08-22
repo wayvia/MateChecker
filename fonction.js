06ed8ef56edd999fecb7f3065b1856d2c9d864fc
wayvia <polo-34-loic@hotmail.fr>
2019-08-22 02:36:14 +0200

    Add files via upload

--- fonction.js
+++ fonction.js
@@ -1,24 +1,47 @@
-
-module.exports = {WelcomeNew, Register, Aide, Lvl, Gear}
-    
-    function WelcomeNew(member) {
-    member.createDM().then(function(Channel){
-        Channel.send('Bienvenue ' + member.displayName + '! Je suis là afin de te guider pour que tu sâches comment utiliser mes fonctions. Fais un !aide pour que je t\'indique les différentes fonctions. N\'hésite pas!')
+function welcomeNew(member) {
+    member.createDM().then(function(channel) {
+        channel.send("```Bienvenue " + member.displayName + "! Je suis là afin de te guider pour que tu sâches comment utiliser mes fonctions. Fais un !aide dans un channel du discord pour que je t'indique les différentes fonctions. N'hésite pas!```")
     })
 }
 
-function Register(member){
-
+function deleteUser(member, server) {
+    server.query("DELETE FROM utilisateurs WHERE ID_PERSO = '" + member.id + "';")
 }
 
-function Aide() {
-
+function register(message, server) {
+    let string = message.content.split(" ")[1].split("-")
+    let serveur = string[1]
+    let pseudo = string[0]
+    let persoId = message.member.id
+    let idserver = message.guild.id
+    server.query("SELECT count(*) as NB FROM utilisateurs where ID_PERSO = '" + persoId + "';", (error, results, fields) => {
+        if (results[0].NB === 0) {
+            server.query("INSERT INTO utilisateurs (PSEUDO, SERVEUR, ID_PERSO, isMain, ID_SERVER) VALUES ('" + pseudo + "','" + serveur + "','" + persoId + "','" + idserver + "',true);")
+            message.guild.members
+                .get(message.member.id)
+                .setNickname(pseudo + "-" + serveur)
+                .then(() => {
+                    message.channel.send(message.member + " Enregistré !")
+                })
+                .catch(() => {
+                    message.channel.send("Il y a eu une erreur !")
+                })
+        } else {
+            server.query("INSERT INTO utilisateurs (PSEUDO, SERVEUR, ID_PERSO, ID_SERVER) VALUES ('" + pseudo + "','" + serveur + "','" + persoId + "','" + idserver + "');")
+        }
+    })
+    message.delete(1000)
 }
 
-function Lvl(member){
-
+function aide(message) {
+    message.member.createDM().then(function(channel) {
+        return channel.send("```Tout d'abords, pour t'enregistrer, tu dois faire !register Tonpseudo-Tonserveur. Attention à l'ortographe c'est important sinon ça ne fonctionnera pas. Ensuite tu as la commande !gear qui te permet de voir en temps réel l'équipement de tout le monde ou d'une personne si tu mets son pseudo après. Tu as la commande !Lvl qui te permet de savoir le niveau de tes collègues ou d'une personne en particulier, si tu mets son pseudo après. Voilà c'est tout pour l'instant.```")
+    })
+    message.delete(1000)
 }
 
-function Gear(member){
+function lvl(member, server, clientbnet) {}
 
-}
+function gear(member, server, clientbnet) {}
+
+module.exports = { welcomeNew, register, aide, lvl, gear, deleteUser }
--- index.js
+++ index.js
@@ -1,27 +1,48 @@
-const Config = require('./config')
-const Discord = require('discord.js')
-const Client = new Discord.Client()
-Client.login(Config.token)
-const Fonction = require('./fonction')
+const config = require("./config")
+const Discord = require("discord.js")
+const client = new Discord.Client()
+client.login(config.token)
+const Fonction = require("./fonction")
+var Mysql = require("mysql")
+server = new Mysql.createConnection(config.connectServ)
+var blizzardclient = require("battlenet.js")
+module.exports = { server }
+var cregion = "eu"
+var clocale = "fr_FR"
 
-Client.on('guildMemberAdd', member =>{
-    Fonction.WelcomeNew(member)
+//console.log(
+client.guilds.forEach(guild => {
+    console.log(guild)
+    server.query("SELECT * from server where ID_SERVER = " + guild.id + ";", (error, results, fields) => {
+        console.log(results)
+        if (results === null || "") {
+            server.query("INSERT INTO server (ID_SERVER) values ('" + guild.id + "')")
+        }
+    })
+})
+//)
+
+var clientbnet = new blizzardclient(config.APIKey, { region: cregion, locale: clocale })
+console.log(clientbnet)
+
+client.on("guildMemberAdd", member => {
+    Fonction.welcomeNew(member, server, clientbnet)
 })
 
-Client.on('message', message =>{
-    if(message.content.startsWith('!')){
-        if(message.content.toLowerCase.startsWith('!aide')){
-            Fonction.Aide(member)
-        }
-        if(message.content.toLowerCase.startsWith('!register')){
-            Fonction.Register(message)
-        }
-        if(message.content.toLowerCase.startsWith('!lvl')){
-            Fonction.Register(message)
-        }
-        if(message.content.toLowerCase.startsWith('!gear')){
-            Fonction.Register(message)
-        }
+client.on("guildMemberRemove", member => {
+    Fonction.deleteUser(member, server)
+})
 
+client.on("message", message => {
+    if (message.content.startsWith("!")) {
+        if (message.content.toLowerCase().startsWith("!aide")) {
+            Fonction.aide(message)
+        } else if (message.content.toLowerCase().startsWith("!register")) {
+            Fonction.register(message, server)
+        } else if (message.content.toLowerCase().startsWith("!lvl")) {
+            Fonction.lvl(message, clientbnet, server)
+        } else if (message.content.toLowerCase().startsWith("!gear")) {
+            Fonction.gear(message, clientbnet, server)
+        } else message.channel.send("Je ne parviens à trouver cette commande ! Vérifie l'orthographe ou tape !aide pour avoir toute les commandes.")
     }
 })
